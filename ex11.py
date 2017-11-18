fac596cc071501bcc2e8c2ee1b10b95ab908bf6f
nagaraja r <r.nagaraja@siemens.com>
2017-11-18 22:48:25 +0530

    remove duplicates

--- ex11.py
+++
@@ -1,7 +0,0 @@
-print ("How old are you ",)
-age = input()
-print ("How tall are you",)
-height = input()
-print ("How much do you wiegh",)
-weight = input()
-print ("So you are %r old, %r tall, %r heavy." %(age, height, weight))
--- ex12.py
+++
@@ -1,4 +0,0 @@
-age = input("How old are you?")
-height = input("How tall are you?")
-weight = input("How much do you weigh?")
-print("So, you are %r age, %r tall and %r heavy" %(age, height,weight))
